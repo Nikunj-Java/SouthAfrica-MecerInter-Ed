f15734eb5fa3588a2e27405ccd87b9c6e52b4110
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2024-07-16 19:28:03 +0530

    Day2 Notes Selenium,Chrome and FireFox Test

--- Day-2/Selenium_Session_2/src/com/selenium/demo/FirefoxTest.java
+++ Day-2/Selenium_Session_2/src/com/selenium/demo/FirefoxTest.java
@@ -1,6 +1,9 @@
 package com.selenium.demo;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,19 +16,32 @@ public class FirefoxTest {
 		//link:https://github.com/mozilla/geckodriver/releases
 		System.setProperty("webdriver.gecko.driver", "D:\\Phase-5\\Drivers2024\\geckodriver-win64\\geckodriver.exe");
 		
-		WebDriver driver= new FirefoxDriver();
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-				 
-		driver.get("https://www.google.com/");
-		
-	    driver.findElement(By.name("q")).sendKeys("Browserstack Guide");
-	    WebElement searchButton=driver.findElement(By.name("btnK"));
-	    //searchButton.click();
-		 
-		 
-		 
-		 driver.quit();
+		WebDriver driver = new FirefoxDriver(); //Creating an object of FirefoxDriver
+		  
+	             try {
+	            	// Maximize the browser window
+	 	            driver.manage().window().maximize();
+	 	            driver.manage().deleteAllCookies();
+	 	             
+	 	            driver.get("https://www.google.com/");
+	 	    
+	 	            WebElement searchBtn = driver.findElement(By.name("q"));
+	 	            searchBtn.sendKeys("Browserstack Guide");
+	 	            searchBtn.sendKeys(Keys.RETURN);
+	 	            
+					Thread.sleep(2000);
+					System.out.println("Title of the Page is: "+driver.getTitle());
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					 
+				}
+	             finally {
+	            	 driver.quit();
+				}
+	             
+	            
+	        
 		 
 		
 	}
